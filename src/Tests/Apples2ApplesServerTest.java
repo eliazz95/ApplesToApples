2c9dbd5ca113aa6e85a5438dce45d3e616ead46d
PointMaker <eliaas_95@hotmail.com>
2021-04-16 19:08:32 +0200

    Create Apples2ApplesServerTest.java

---
+++ src/Tests/Apples2ApplesServerTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class Apples2ApplesServerTest {
+
+
+    /*@Test
+    @DisplayName("Rule 1 | Read all green apples from file")
+    void readAllGreenApples(){
+
+        Apples2ApplesServer.setupCards();
+        int deckSize = Apples2ApplesServer.greenDeck.deckSize();
+        assertEquals(614, deckSize);
+
+    }
+
+    @Test
+    @DisplayName("Rule 2 | Read all red apples from file")
+    void readAllRedApples(){
+
+        // Make sure to change the game settings to disable any card variation before doing this test
+        Apples2ApplesServer.setupCards();
+        int deckSize = Apples2ApplesServer.redDeck.deckSize();
+        assertEquals(1826, deckSize);
+
+    }
+
+    @Test
+    @DisplayName("Rule 3 | Shuffle both decks")
+    void shuffleBothDecks(){
+
+        Apples2ApplesServer.setupCards();
+
+        // Check that the first red card is not equal to the first red card of an unshuffled deck
+        assertNotEquals("[A Bad Haircut] - The perfect start to a bad hair day. ", Apples2ApplesServer.redDeck.drawCard());
+
+        // Check that the first green card is not equal to the first green card of an unshuffled deck
+        assertNotEquals("[A Bad Haircut] - The perfect start to a bad hair day. ", Apples2ApplesServer.greenDeck.drawCard());
+    }
+
+    @Test
+    @DisplayName("Rule 4 | Deal seven cards to each player")
+    void dealSevenCardsToPlayers(){
+        Apples2ApplesServer.setupCards();
+        Apples2ApplesServer.startServer(2050, 1);
+
+
+    }*/
+
+}
+

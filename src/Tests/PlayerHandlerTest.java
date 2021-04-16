f869fd334b35384a89423b6f6146af9bf9d8beea
PointMaker <eliaas_95@hotmail.com>
2021-04-16 19:08:28 +0200

    Create PlayerHandlerTest.java

---
+++ src/Tests/PlayerHandlerTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerHandlerTest {
+
+    RedApplesDeck redDeck = new RedApplesDeck(false, false);
+
+    @Test
+    @DisplayName("Rule 4 | Deal seven cards to each player")
+    void dealSevenCardsToPlayers(){
+
+    }
+
+}
